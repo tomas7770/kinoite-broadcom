2b8c2539e0a1ba9f2521a1eec07ad939ccda9ec9
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-12-26 20:41:25 +0000

    Remove kmod install, add RPMFusion
    The kmod is already installed, RPMFusion is missing for broadcom-wl

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
+# Install RPMFusion
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-find /tmp/rpms
-rpm-ostree install /tmp/rpms/ublue-os/ublue-os-akmods*.rpm
-rpm-ostree install /tmp/rpms/kmods/kmod-wl*.rpm
 rpm-ostree install broadcom-wl
