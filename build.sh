2f63a3b0ef7a7bdffcb0488ae140b5952eea956e
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2025-04-20 15:09:56 +0100

    Upgrade to Fedora 41

--- build.sh
+++ build.sh
@@ -9,12 +9,10 @@ RELEASE="$(rpm -E %fedora)"
 
 # Packages can be installed from any enabled yum repo on the image.
 # Install RPMFusion
-rpm-ostree install \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
 
 find /tmp/rpms
-rpm-ostree install /tmp/rpms/kmods/kmod-wl*.rpm
-rpm-ostree install broadcom-wl
+dnf install -y /tmp/rpms/kmods/kmod-wl*.rpm
+dnf install -y broadcom-wl
